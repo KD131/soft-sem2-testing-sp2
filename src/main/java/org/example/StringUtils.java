46be89486d7c0fbf1808acb7dbbf20b05d33fd0d
KD131 <71063280+KD131@users.noreply.github.com>
2023-10-11 21:39:17 +0200

    string reversal step definitions fail tests

---
+++ src/main/java/org/example/StringUtils.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class StringUtils {
+    public static String reverse(String input) {
+        return null;
+    }
+}
--- src/test/java/org.example/StringReversalStepdefs.java
+++ src/test/java/org.example/StringReversalStepdefs.java
@@ -4,17 +4,24 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringReversalStepdefs {
+    private String stringToReverse;
+    private String reversedString;
+
     @Given("I have a string {string}")
     public void iHaveAString(String arg0) {
-
+        stringToReverse = arg0;
     }
 
     @When("I reverse the string")
     public void iReverseTheString() {
+        reversedString = StringUtils.reverse(stringToReverse);
     }
 
     @Then("I should get {string}")
     public void iShouldGet(String arg0) {
+        assertEquals(arg0, reversedString);
     }
 }
